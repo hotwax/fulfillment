d394e2b136056e1cb66ed28084f824bba55c19c8
Yash Maheshwari <yashmaheshwari551@gmail.com>
2021-12-30 12:41:47 +0530

    Updated: condition to always display the maximum available shipping methods(#1wut90a)

--- src/store/modules/order/actions.ts
+++ src/store/modules/order/actions.ts
@@ -11,14 +11,15 @@ import * as types from './mutation-types'
 const actions: ActionTree<OrderState, RootState> = {
 
   // get open orders
-  async fetchOpenOrders ({ commit }, payload) {
+  async fetchOpenOrders ({ commit, state }, payload) {
     emitter.emit('presentLoader');
     let resp;
 
     try {
       resp = await OrderService.fetchOpenOrders(payload);
       if (resp.status === 200 && resp.data.grouped.orderId.matches > 0 && !hasError(resp)) {
-        commit(types.ORDER_OPEN_UPDATED, {open: resp.data.grouped.orderId.groups, total: resp.data.grouped.orderId.matches, shipmentMethods: resp.data.facets.shipmentMethodTypeIdFacet.buckets})
+        const shipmentMethods = state.shipmentMethods.length ? state.shipmentMethods.length < resp.data.facets.shipmentMethodTypeIdFacet.buckets.length ? resp.data.facets.shipmentMethodTypeIdFacet.buckets : state.shipmentMethods : resp.data.facets.shipmentMethodTypeIdFacet.buckets
+        commit(types.ORDER_OPEN_UPDATED, {open: resp.data.grouped.orderId.groups, total: resp.data.grouped.orderId.groups.length, shipmentMethods})
         this.dispatch('product/getProductInformation', {orders: resp.data.grouped.orderId.groups})
       } else {
         showToast(translate('Something went wrong'))
