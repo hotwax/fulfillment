063c449661d48e371b9d04c81da18767a50b7a72
Disha <dishatalreja1202@gmail.com>
2022-01-31 13:51:37 +0530

    Implemented: store and service for the order state(#201pbe2)

---
+++ src/services/OrderService.ts
@@ -0,0 +1,13 @@
+import api from '@/api';
+
+const fetchInProgressOrders = async (query: any): Promise <any>  => {
+  return api({
+    url: "/solr-query", 
+    method: "get",
+    data: query
+  });
+}
+
+export const OrderService = {
+    fetchInProgressOrders
+} 
