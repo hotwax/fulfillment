5eb94363b15f5f8c3841d6d93ccd5d51971e944e
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-02-24 12:29:00 +0530

    Improved: method names, added some comments and handled some cases of generating and selecting picklist size when changing shipping methods(#1wut90a)

--- src/components/PicklistSize.vue
+++ src/components/PicklistSize.vue
@@ -8,11 +8,11 @@
 
     <ion-content>
       <ion-list>
-        <ion-radio-group v-model="size" @ionChange="setPicklistSize()">
-          <ion-item v-for="count in preparePicklistSize()" :key="count">
+        <ion-radio-group :value="currentPicklistSize" @ionChange="setPicklistSize($event.detail.value)">
+          <ion-item v-for="count in preparePicklistSizeOptions()" :key="count">
             <ion-radio slot="start" :value="count"/>
             <!-- If the count is greater then length of openOrders then display the length of total orders -->
-            <ion-label>{{ count >= openOrders.total ? openOrders.total : count }} {{ $t('orders') }}</ion-label>
+            <ion-label>{{ count }} {{ $t('orders') }}</ion-label>
             <!-- TODO: add support to display the order items count -->
             <!-- <ion-note slot="end">10 items</ion-note> -->
           </ion-item>
@@ -59,22 +59,33 @@ export default defineComponent({
       currentPicklistSize: 'picklist/getPicklistSize'
     })
   },
-  data () {
-    return {
-      size: 10
-    }
-  },
   methods: {
-    preparePicklistSize () {
+    preparePicklistSizeOptions () {
       // creating an array of numbers using Array.keys method and then multiplying each by 5
-      return [ ...Array(Math.ceil(this.openOrders.total / 5)).keys() ].map( i => {
+      const picklistSizeOptions =  [ ...Array(Math.ceil(this.openOrders.total / 5)).keys() ].map( i => {
         const count = (i+1) * 5
         // added check that if the count is greater than the total orders available then assigning orders total as picklistSize
         return count > this.openOrders.total ? this.openOrders.total : count
       })
+
+      // Added this check as if the picklistSize options only have a single option to select then by default selecting that option
+      // This condition will become true only in case when the orders total is equal to or less than 5, as we are generating picklist options with a difference of 5
+      if(picklistSizeOptions.length === 1) {
+        this.setPicklistSize(picklistSizeOptions[0])
+      } else {
+        // Checking that whether the current picklist size exists in the options available
+        // This scenario occurs when we have selected a shipping method having for example 3 orders, so we set the picklist size to 3
+        // Now when we de-select the shipping method or select another shipping method than the orders totals will increase,
+        // and thus 3 as an picklist option will not be available, thus checking for below condition
+
+        // Using + operator to convert string into number, as env entries always return a string
+        this.setPicklistSize(picklistSizeOptions.includes(this.currentPicklistSize) ? this.currentPicklistSize : +process.env.VUE_APP_VIEW_SIZE)
+      }
+
+      return picklistSizeOptions;
     },
-    setPicklistSize () {
-      this.store.dispatch('picklist/setPicklistSize', this.size)
+    async setPicklistSize(size: number) {
+      await this.store.dispatch('picklist/setPicklistSize', size)
       // closing the menu after selecting any picklist size
       menuController.close()
     }
