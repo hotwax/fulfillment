7b640f8f4afb885acb7136207db36e2584586fc3
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-02-27 11:32:55 +0530

    Implemented: order service and stock service for defining endpoints(#1y8mznd)

---
+++ src/services/OrderService.ts
@@ -0,0 +1,22 @@
+import { api } from '@/adapter';
+
+const fetchInProgressOrders = async (query: any): Promise <any>  => {
+  return api({
+    url: "solr-query", 
+    method: "post",
+    data: query
+  });
+}
+
+const packOrder = async (query: any): Promise <any> => {
+  return api({
+    url: "/service/packStoreFulfillmentOrder",
+    method: "post",
+    data: query
+  })
+}
+
+export const OrderService = {
+  fetchInProgressOrders,
+  packOrder
+} 
