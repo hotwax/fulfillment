35001d0d24b7972f2a41f68aded79b9a30b99a3c
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-03-14 14:52:49 +0530

    Implemented: method to create picklist for displayed orders(#1wut90a)

--- src/views/AssignPickerModal.vue
+++ src/views/AssignPickerModal.vue
@@ -59,6 +59,7 @@ import { mapGetters, useStore } from "vuex";
 import { hasError, showToast } from "@/utils";
 import { translate } from "@/i18n";
 import { PicklistService } from "@/services/PicklistService";
+import emitter from "@/event-bus";
 
 export default defineComponent({
   name: "AssignPickerModal",
@@ -81,7 +82,8 @@ export default defineComponent({
   },
   computed: {
     ...mapGetters({
-      openOrders: 'order/getOpenOrders'
+      openOrders: 'order/getOpenOrders',
+      currentFacility: 'user/getCurrentFacility'
     })
   },
   data () {
@@ -111,14 +113,56 @@ export default defineComponent({
       this.pickers = []
       this.fetchPickers()
     },
-    printPicklist () {
-      // TODO: update API support to create a picklist
-      const payload = this.openOrders;
-      if (this.selectedPickers.length) {
-        this.store.dispatch('picklist/createPicklist', payload)
-      } else {
+    async printPicklist () {
+      if (!this.selectedPickers.length) {
         showToast(translate('Select a picker'))
+        return;
       }
+
+      emitter.emit("presentLoader")
+      let resp;
+
+      const orders = this.state.order.open.list;
+      const items = []
+
+      const formData = new FormData();
+      formData.append("facilityId", this.currentFacility.facilityId);
+      orders.map((order) => {
+        order.doclist.docs.map((item) => items.push(item))
+      });
+
+      items.map((item, index) => {
+        formData.append("facilityId_o_"+index, this.currentFacility.facilityId)
+        formData.append("shipmentMethodTypeId_o_"+index, item.shipmentMethodTypeId)
+        formData.append("itemStatusId_o_"+index, "PICKITEM_PENDING")
+        formData.append("shipGroupSeqId_o_"+index, item.shipGroupSeqId)
+        formData.append("orderId_o_"+index, item.orderId)
+        formData.append("orderItemSeqId_o_"+index, item.orderItemSeqId)
+        formData.append("productId_o_"+index, item.productId)
+        formData.append("quantity_o_"+index, item.itemQuantity)
+        formData.append("inventoryItemId_o_"+index, item.inventoryItemId)
+        formData.append("picked_o_"+index, item.itemQuantity)
+        formData.append("_rowSubmit_o_"+index, "Y")
+      })
+
+      // Adding all the pickers selected in FormData
+      // TODO: check if we need to only allow selection of 3 or less pickers as in the current fulfillment app we can only select 3 pickers
+      this.selectedPickers.map((picker) => formData.append("pickerIds", picker.id))
+
+      try {
+        resp = await PicklistService.createPicklist(formData);
+        if (resp.status === 200 && !hasError(resp)) {
+          this.closeModal();
+          await this.store.dispatch('order/fetchOpenOrders')
+        } else {
+          showToast(translate('Failed to create picklist for orders'))
+        }
+      } catch (err) {
+        console.error(err)
+        showToast(translate('Something went wrong'))
+      }
+
+      emitter.emit("dismissLoader")
     },
     async fetchPickers() {
       let inputFields = {}
