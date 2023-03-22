b35c12a189d1baef11d5c976ab79c6a475643c66
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-03-22 17:35:01 +0530

    Removed: unwanted todos(#85zrrt9a0)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -352,7 +352,6 @@ export default defineComponent({
               })
 
               if(!hasError(resp)) {
-                // TODO: update toast messages for success and failure case
                 showToast(translate('Recycling has been started. All outstanding orders will be recycled shortly.'))
               } else {
                 showToast(translate('Failed to recycle outstanding orders'))
@@ -388,7 +387,6 @@ export default defineComponent({
               })
 
               if(!hasError(resp)) {
-                // TODO: update toast messages for success and failure case
                 showToast(translate('Recycling has been started. All in progress orders will be recycled shortly.'))
               } else {
                 showToast(translate('Failed to recycle in progress orders'))
