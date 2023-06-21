40ee8d518ccd48fcae15e54ecb38991597e5f6bf
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-06-21 12:39:32 +0530

    Implemented: logic to upload the json file for import orders and updated en.json file for static text(#149)

--- src/views/UploadImportOrders.vue
+++ src/views/UploadImportOrders.vue
@@ -38,12 +38,15 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { mapGetters, useStore } from "vuex";
+import { useStore } from "vuex";
 import { useRouter } from 'vue-router';
-import { IonPage, IonSelect, IonSelectOption, IonHeader, IonList, IonListHeader, IonToolbar, IonBackButton, IonTitle, IonContent, IonItem, IonLabel, IonButton } from '@ionic/vue'
+import { IonPage, IonSelect, IonSelectOption, IonHeader, IonList, IonListHeader, IonToolbar, IonBackButton, IonTitle, IonContent, IonItem, IonLabel, IonButton, alertController } from '@ionic/vue'
 import { ellipsisVerticalOutline, businessOutline, shirtOutline, sendOutline, checkboxOutline, calculatorOutline, cloudUploadOutline, arrowUndoOutline, chevronForwardOutline, timeOutline } from 'ionicons/icons'
 import { parseCsv, showToast } from '@/utils';
 import { translate } from "@/i18n";
+import { UploadService } from "@/services/UploadService"
+import { hasError } from '@/adapter';
+import logger from '@/logger';
 
 export default defineComponent({
   name: 'UploadImportOrders',
@@ -61,16 +64,6 @@ export default defineComponent({
     IonItem,
     IonLabel,
     IonButton
-  },
-  computed: {
-    ...mapGetters({
-      facilities: 'util/getFacilities',
-      purchaseOrders: 'order/getPurchaseOrders',
-      getProduct: 'product/getProduct',
-      instanceUrl: 'user/getInstanceUrl',
-      dateTimeFormat : 'user/getPreferredDateTimeFormat',
-      fileName: 'order/getFileName'
-    })
   },
   data() {
     return {
@@ -91,44 +84,12 @@ export default defineComponent({
     // this.$refs.file.value = null;
   },
   methods: {
-    async parseCsv(file: any) {
-      // TODO: check if we need this check as this method is only called when we have file
-      if (file) {
-        // TODO: check what happens when parseCSV rejects
-        const csvData = await parseCsv(file).then(res => {
-          return res;
-        })
-        // TODO: check why we have multiple toast on success
-        showToast(translate("File uploaded successfully"));
-        return csvData;
-      }
-      showToast(translate("Something went wrong, Please try again")); 
-    },
-    mapFields(mapping: any) {
-      const fieldMapping = JSON.parse(JSON.stringify(mapping));
-
-      // TODO: Store an object in this.content variable, so everytime when accessing it, we don't need to use 0th index
-      const csvFields = Object.keys(this.content[0]);
-
-      // const missingFields = Object.values(fieldMapping.value).filter((field: string) => {
-      //   if(!csvFields.includes(field)) return field;
-      // });
-
-      // if(missingFields.length) showToast(translate("Some of the mapping fields are missing in the CSV: ", { missingFields: missingFields.join(", ") }))
-
-      Object.keys(fieldMapping.value).map((key) => {
-        if(!csvFields.includes(fieldMapping.value[key])){
-          fieldMapping.value[key] = "";
-        }
-      })
-      this.fieldMapping = fieldMapping.value;
-    },
     async parse(event: any) {
       const file = event.target.files[0];
       try {
         if (file) {
           this.file = file;
-          this.content = await this.parseCsv(this.file);
+          this.content = await parseCsv(file).then(res => res);
           this.fileColumns = Object.keys(this.content[0]);
           showToast(translate("File uploaded successfully"));
         } else {
@@ -136,30 +97,59 @@ export default defineComponent({
         }
       } catch {
         this.content = []
-        showToast(translate("Please upload a valid reset inventory csv to continue"));
+        showToast(translate("Please upload a valid csv to continue"));
       }
     },
-    save() {
+    async save() {
       const areAllFieldsSelected = Object.values(this.fieldMapping).every(field => field !== "");
       
       if (!areAllFieldsSelected) {
         showToast(translate("Select all the fields to continue"));
         return;
-      } 
+      }
 
-      const stockItems = this.content.map((item: any) => {
-        return {
-          shopifyProductSKU: item[this.fieldMapping.productSku],
-          quantity: item[this.fieldMapping.quantity],
-          facilityId: '',
-          externalFacilityId: item[this.fieldMapping.facility],
-          locationSeqId: item[this.fieldMapping.locationSeqId]
-        }
-      })
-      this.store.dispatch('stock/processUpdateStockItems', stockItems);
-      this.router.push({
-        name:'InventoryDetail'
-      })
+      const uploadData = this.content.map((order: any) => ({
+        orderId: order[this.fieldMapping.orderId],
+        facilityId: order[this.fieldMapping.facilityId],
+        trackingCode: order[this.fieldMapping.trackingCode]
+      }))
+
+      const fileName = this.file.name.replace(".csv", ".json");
+      const params = {
+        "configId": "IMP_TRACK_NUM"
+      }
+      const alert = await alertController.create({
+        header: this.$t("Import shipped orders"),
+        message: this.$t("Make sure all the mappings you have selected is correct."),
+        buttons: [{
+          text: this.$t("Cancel"),
+          role: 'cancel',
+        }, {
+          text: this.$t("Upload"),
+          handler: async () => {
+            try {
+              const resp = await UploadService.uploadJsonFile(UploadService.prepareUploadJsonPayload({
+                uploadData,
+                fileName,
+                params
+              }))
+
+              if(hasError(resp)) {
+                logger.error(resp.data)
+                showToast('Configuration Missing')
+                return;
+              }
+
+              showToast(translate("The import orders has been uploaded successfully"))
+              this.router.back(); // go back to previous route
+            } catch(err) {
+              logger.error(err)
+              showToast(translate("Something went wrong, please try again"));
+            }
+          }
+        }]
+      });
+      return alert.present();
     }
   },
   setup() {
