cc2baf0cdacb97943ca677b134da7fca1666d7ec
k2maan <ketuman333@gmail.com>
2023-07-19 18:56:45 +0530

    Implemented: permissions management (#194)

--- src/store/modules/user/UserState.ts
+++ src/store/modules/user/UserState.ts
@@ -1,6 +1,7 @@
 export default interface UserState {
     token: string;
     current: object | null;
+    permissions: any;
     currentFacility: object;
     instanceUrl: string;
     currentEComStore: object;
