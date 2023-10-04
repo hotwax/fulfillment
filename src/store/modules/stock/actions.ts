844a72b09d96c9ce5755bce9632371d612ef03fe
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-10-04 19:22:51 +0530

    Improved: state to remove unwanted actions, and mutations and updated the getter to return empty object when product info not found in state(#182)

--- src/store/modules/stock/actions.ts
+++ src/store/modules/stock/actions.ts
@@ -5,6 +5,8 @@ import StockState from './StockState'
 import * as types from './mutation-types'
 import { hasError } from '@/adapter'
 import logger from '@/logger'
+import { showToast } from '@/utils'
+import { translate } from '@/i18n'
 
 const actions: ActionTree<StockState, RootState> = {
   async fetchStock({ commit }, { productId }) {
@@ -17,33 +19,15 @@ const actions: ActionTree<StockState, RootState> = {
 
       const resp: any = await StockService.getInventoryAvailableByFacility(payload);
 
-      if (resp.status === 200 && !hasError(resp)) {
+      if (!hasError(resp)) {
         commit(types.STOCK_ADD_PRODUCT, { productId: payload.productId, stock: resp.data })
+      } else {
+        throw resp.data;
       }
     } catch(err) {
       logger.error(err)
+      showToast(translate('No data available!'))
     }
-  },
-
-  /**
-  * Add stocks of list of products
-  */
-  async addProducts({ dispatch, state }, { productIds }) {
-    const cachedProductIds = Object.keys(state.products);
-    const productIdFilter= productIds.reduce((filter: Array<string>, productId: any) => {
-      // If product not available in cached products then only fetch the stock for that product
-      if (productId && !cachedProductIds.includes(productId)) {
-        filter.push(productId)
-      }
-
-      return filter
-    }, []);
-
-    // If there are no products skip the API call
-    if (!productIdFilter.length) return;
-
-    dispatch('fetchStock', { productIds: productIdFilter })
   }
-
 }
 export default actions;
--- src/store/modules/stock/mutations.ts
+++ src/store/modules/stock/mutations.ts
@@ -5,11 +5,6 @@ import * as types from './mutation-types'
 const mutations: MutationTree <StockState> = {
   [types.STOCK_ADD_PRODUCT] (state, payload) {
     state.products[payload.productId] = payload.stock
-  },
-  [types.STOCK_ADD_PRODUCTS] (state, payload) {
-    payload.products.forEach((product: any) => {
-      state.products[product.productId] = product.atp
-    });
   }
 }
 export default mutations;
