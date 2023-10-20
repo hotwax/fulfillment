6dd82f47e762d20983d33b2a406041fde3bdf0fc
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-10-20 19:34:28 +0530

    Fixed: issue in shipping address data on initial fetch related to undefined and updated the actions to update state in sync(#313)

--- src/views/ShippingDetails.vue
+++ src/views/ShippingDetails.vue
@@ -10,15 +10,15 @@
         <ion-label>
           <h3>{{ currentOrder?.shippingAddress?.toName }}</h3>
           <p>{{ currentOrder?.shippingAddress?.address1 }}</p>
-          <p v-if="currentOrder?.shippingAddress?.address2">{{ currentOrder?.shippingAddress?.address2 }}</p>
-          <p>{{ currentOrder?.shippingAddress?.city ? currentOrder?.shippingAddress?.city + "," : "" }} {{ currentOrder.shippingAddress?.zipCode }}</p>
-          <p>{{ currentOrder?.shippingAddress?.stateName ? currentOrder?.shippingAddress?.stateName + "," : "" }} {{ currentOrder.shippingAddress?.countryName }}</p>
+          <p v-if="currentOrder?.shippingAddress?.address2">{{ currentOrder.shippingAddress.address2 }}</p>
+          <p>{{ currentOrder?.shippingAddress?.city ? currentOrder.shippingAddress.city + "," : "" }} {{ currentOrder.shippingAddress?.zipCode }}</p>
+          <p>{{ currentOrder?.shippingAddress?.stateName ? currentOrder.shippingAddress.stateName + "," : "" }} {{ currentOrder.shippingAddress?.countryName }}</p>
         </ion-label>
       </ion-item>
       <ion-item color="light" lines="none">
         <ion-label class="ion-text-wrap">
           <p class="overline">{{ translate("Handling Instructions") }}</p>
-          <p>{{ currentOrder?.shippingInstructions ? currentOrder?.shippingInstructions : 'Sample Handling instructions' }}</p>
+          <p>{{ currentOrder?.shippingInstructions ? currentOrder.shippingInstructions : 'Sample Handling instructions' }}</p>
         </ion-label>
       </ion-item>
       <ion-item lines="none" v-if="currentOrder.trackingCode">
@@ -26,7 +26,7 @@
           {{ currentOrder.trackingCode }}
         </ion-label>        
         <ion-button fill="clear" @click="printShippingLabel(currentOrder)">
-          <ion-icon :icon="openOutline" slot="end"></ion-icon>
+          <ion-icon :icon="openOutline" slot="end" />
         </ion-button>
       </ion-item>
       <ion-item lines="none" v-if="!currentOrder.trackingCode && ['PICKITEM_PICKED', 'PICKITEM_COMPLETED'].includes(currentOrder?.items[0]?.picklistItemStatusId)">
@@ -37,7 +37,7 @@
           {{ translate('No carrier error') }}
         </ion-label>
         <ion-button fill="clear" @click="retryShippingLabel(currentOrder)">
-          <ion-icon :icon="refreshSharp" slot="end" ></ion-icon>
+          <ion-icon :icon="refreshSharp" slot="end" />
         </ion-button>
       </ion-item>
     </ion-card>
@@ -45,80 +45,82 @@
 </template>
 
 <script lang="ts">
-  import { 
-    IonLabel,
-    IonItem,
+import {
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonIcon,
+  IonItem,
+  IonLabel
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { openOutline, refreshSharp } from "ionicons/icons";
+import { useStore, mapGetters } from "vuex";
+import { showToast } from '@/utils';
+import { translate } from '@hotwax/dxp-components'
+import { OrderService } from '@/services/OrderService';
+import { hasError } from '@/adapter'
+
+export default defineComponent({
+  name: "CreateMappingModal",
+  components: {
+    IonButton,
     IonCard,
     IonCardHeader,
-    IonCardTitle
-  } from "@ionic/vue";
-  import { defineComponent } from "vue";
-  import { openOutline, refreshSharp } from "ionicons/icons";
-  import { useStore, mapGetters } from "vuex";
-  import { showToast } from '@/utils';
-  import { translate } from '@hotwax/dxp-components'
-  import { OrderService } from '@/services/OrderService';
-  import { hasError } from '@/adapter'
-
-  export default defineComponent({
-    name: "CreateMappingModal",
-    components: { 
-      IonLabel,
-      IonItem,
-      IonCard,
-      IonCardHeader,
-      IonCardTitle
-    },
-    data() {
-      return {
-        shipmentLabelErrorMessages: ""
-      }
-    },
-    computed: {
-      ...mapGetters({
-        currentOrder: 'order/getCurrent'
-      })
-    },
-    async mounted() {
-      // Fetching shipment label errors
-      const shipmentIds = this.currentOrder.shipments.map((shipment: any) => shipment.shipmentId);
-      const labelErrors = await OrderService.fetchShipmentLabelError(shipmentIds);
-      this.shipmentLabelErrorMessages = labelErrors.join(', ');
-    },
-    methods: {
-      async printShippingLabel(order: any) {
-        const shipmentIds = order.shipments.map((shipment: any) => shipment.shipmentId)
-        await OrderService.printShippingLabel(shipmentIds)
-      },
-      async retryShippingLabel(order: any) {
-        const shipmentIds = order.shipmentPackages.map((shipmentPackage: any) => shipmentPackage.shipmentId);
-        const resp = await OrderService.retryShippingLabel(shipmentIds)
-        if (!hasError(resp)) {
-          showToast(translate("Shipping Label generated successfully"))
-          await this.printShippingLabel(order)
-        } else {
-          showToast(translate("Failed to generate shipping label"))
-        }
-      }
-    },
-    setup() {
-      const store = useStore();
-      return {
-        openOutline,
-        refreshSharp,
-        translate,
-        store
-      };
+    IonCardTitle,
+    IonIcon,
+    IonItem,
+    IonLabel
+  },
+  data() {
+    return {
+      shipmentLabelErrorMessages: ""
     }
-  });
-  </script>
-
-  <style>
-
-  .shipgroup-details {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(343px, 1fr));
-    gap: 10px;
+  },
+  computed: {
+    ...mapGetters({
+      currentOrder: 'order/getCurrent'
+    })
+  },
+  async mounted() {
+    // Fetching shipment label errors
+    const shipmentIds = this.currentOrder.shipments?.map((shipment: any) => shipment.shipmentId);
+    const labelErrors = await OrderService.fetchShipmentLabelError(shipmentIds);
+    this.shipmentLabelErrorMessages = labelErrors.join(', ');
+  },
+  methods: {
+    async printShippingLabel(order: any) {
+      const shipmentIds = order.shipments.map((shipment: any) => shipment.shipmentId)
+      await OrderService.printShippingLabel(shipmentIds)
+    },
+    async retryShippingLabel(order: any) {
+      const shipmentIds = order.shipmentPackages.map((shipmentPackage: any) => shipmentPackage.shipmentId);
+      const resp = await OrderService.retryShippingLabel(shipmentIds)
+      if (!hasError(resp)) {
+        showToast(translate("Shipping Label generated successfully"))
+        await this.printShippingLabel(order)
+      } else {
+        showToast(translate("Failed to generate shipping label"))
+      }
+    }
+  },
+  setup() {
+    const store = useStore();
+    return {
+      openOutline,
+      refreshSharp,
+      translate,
+      store
+    };
   }
+});
+</script>
 
-  </style>
+<style>
+.shipgroup-details {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(343px, 1fr));
+  gap: 10px;
+}
+</style>
