d383d9f3bd5ad9693972de21dc14ae4cf902dfa2
shanmukhdutt <ksssaidutt@gmail.com>
2023-12-12 16:17:42 +0530

    Fixed: Indentation issue in ImageModal

--- src/components/ImageModal.vue
+++ src/components/ImageModal.vue
@@ -1,62 +1,62 @@
 <template>
-	<ion-page>
-		<ion-header>
-			<ion-toolbar>
-				<ion-buttons slot="start">
-					<ion-button @click="closeModal">
-						<ion-icon slot="icon-only" :icon="closeOutline" />
-					</ion-button>
-					<ion-title>{{ virtualProductName }}</ion-title>
-				</ion-buttons>
-			</ion-toolbar>
-		</ion-header>
-		<ion-content>
-			<img :src="imageUrl" />
-		</ion-content>
-	</ion-page>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button @click="closeModal">
+            <ion-icon slot="icon-only" :icon="closeOutline" />
+          </ion-button>
+          <ion-title>{{ virtualProductName }}</ion-title>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <img :src="imageUrl" />
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { closeOutline } from "ionicons/icons";
 import {
-	IonButton,
-	IonButtons,
-	IonContent,
-	IonHeader,
-	IonIcon,
-	IonPage,
-	IonTitle,
-	IonToolbar,
-	modalController
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  modalController
 } from '@ionic/vue';
 export default defineComponent({
-	name: 'DisplayImg',
-	components: {
-		IonButton,
-		IonButtons,
-		IonContent,
-		IonHeader,
-		IonIcon,
-		IonPage,
-		IonTitle,
-		IonToolbar
-	},
-	props: {
-		imageUrl: String,
-		virtualProductName: String
-	},
-	methods: {
-		closeModal() {
-			modalController.dismiss({
-				dismissed: true
-			})
-		}
-	},
-	setup() {
-		return {
-			closeOutline
-		}
-	}
+  name: 'DisplayImg',
+  components: {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  },
+  props: {
+    imageUrl: String,
+    virtualProductName: String
+  },
+  methods: {
+    closeModal() {
+      modalController.dismiss({
+        dismissed: true
+      })
+    }
+  },
+  setup() {
+    return {
+      closeOutline
+    }
+  }
 });
 </script>
