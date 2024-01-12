7fa227fd565e78568fec8f71cdcd3c287abfbf15
shanmukhdutt <ksssaidutt@gmail.com>
2024-01-12 11:53:58 +0530

    Displayed: No Variance Rejection Reasons for Order Items(#292)

--- src/store/modules/util/actions.ts
+++ src/store/modules/util/actions.ts
@@ -12,7 +12,7 @@ const actions: ActionTree<UtilState, RootState> = {
     try {
       const payload = {
         "inputFields": {
-          "parentEnumTypeId": "REPORT_AN_ISSUE"
+          "parentEnumTypeId": ["REPORT_AN_ISSUE", "RPRT_NO_VAR_LOG"],
         },
         "fieldList": ["enumId", "description"],
         "distinct": "Y",
