53b739661db8a6dade53162ac0ca8e083caadd12
shanmukhdutt <ksssaidutt@gmail.com>
2024-02-01 10:11:34 +0530

    Improved: Indentation

--- src/services/PartyService.ts
+++ src/services/PartyService.ts
@@ -1,13 +1,13 @@
 import { api } from '@/adapter';
 
 const fetchPartyInformation = async (query: any): Promise <any>  => {
-    return api({
-      url: "performFind",
-      method: "get",
-      params: query
-    });
-  }
+  return api({
+    url: "performFind",
+    method: "get",
+    params: query
+  });
+}
 
-  export const partyService = {
-    fetchPartyInformation
-  }
+export const partyService = {
+  fetchPartyInformation
+}
