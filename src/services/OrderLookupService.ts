1d2b79935d553cc325408e09a75dd04f65214bf3
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-05-08 19:23:21 +0530

    Implemented: services for fetching order lookup information(#511)

--- src/services/OrderLookupService.ts
+++ src/services/OrderLookupService.ts
@@ -1,1 +1,22 @@
-export const OrderService = {}
+import { api } from "@/adapter"
+
+const findOrder = async (payload: any): Promise<any> => {
+  return api({
+    url: "/solr-query",
+    method: "post",
+    data: payload
+  });
+}
+
+const performFind = async (payload: any): Promise<any> => {
+  return api({
+    url: "/performFind",
+    method: "post",
+    data: payload
+  });
+}
+
+export const OrderLookupService = {
+  findOrder,
+  performFind
+}
