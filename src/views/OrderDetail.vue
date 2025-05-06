83ab55431f98ec8a6b0f258d162867a5d8568851
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2025-05-06 15:32:46 +0530

    Fixed: Category tag in other shipments section.

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -314,7 +314,18 @@
                 <ion-card-title>{{ shipGroup.facilityName }}</ion-card-title>
                 {{ shipGroup.shipGroupSeqId }}
               </div>
-              <ion-badge :color="shipGroup.category ? 'primary' : 'medium'">{{ shipGroup.category ? shipGroup.category : translate('Pending allocation') }}</ion-badge>
+              <ion-badge :color="shipGroup.category ? 'primary' : 'medium'">
+                {{
+                  shipGroup.category
+                    ? shipGroup.category === 'open'
+                      ? translate('Open')
+                      : shipGroup.category === 'in-progress'
+                        ? translate('In Progress')
+                        : translate('Completed')
+                    : translate('Pending allocation')
+                }}
+              </ion-badge>
+
             </ion-card-header>
     
             <ion-item v-if="shipGroup.carrierPartyId">
