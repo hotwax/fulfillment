145dd6328ad3edad3bb5a14a09eab3645614125d
Aashutosh Soni <aashutosh.soni@hotwaxsystems.com>
2025-05-31 12:35:48 +0530

    Implemented: added a selector to select static statusFlowId, added handling for empty case and locales. (#1189)

--- src/views/CreateTransferOrder.vue
+++ src/views/CreateTransferOrder.vue
@@ -65,6 +65,11 @@
               <ion-card-title>{{ translate("Plan") }}</ion-card-title>
             </ion-card-header>
             <ion-item>
+              <ion-select :label="translate('Order Type')" placeholder="Select" v-model="currentOrder.statusFlowId">
+                <ion-select-option v-for="(value, label) in StatusFlowIds" :key="label" :value="value">{{ translate(label) }}</ion-select-option>
+              </ion-select>
+            </ion-item>
+            <ion-item>
               <ion-label>{{ translate("Ship Date") }}</ion-label>
               <ion-button slot="end" class="date-time-button" @click="openDateTimeModal('shipDate')">{{ currentOrder.shipDate ? formatDateTime(currentOrder.shipDate) : translate("Select date") }}</ion-button>
             </ion-item>
@@ -241,8 +246,14 @@ const currentOrder = ref({
   destinationFacilityId: "",
   carrierPartyId: "",
   shipmentMethodTypeId: "", 
-  items: []
+  items: [],
+  statusFlowId: "",
 }) as any;
+const StatusFlowIds = {
+  "Fulfill & Receive" : "TO_Fulfill_And_Receive",
+  "Fulfill only" : "TO_Fulfill_Only",
+  "Receive only" : "TO_Receive_Only"
+}
 
 let content = ref([]) as any 
 let fileColumns = ref([]) as any 
@@ -600,6 +611,11 @@ async function createOrder() {
     return;
   }
 
+  if(!currentOrder.value.statusFlowId?.length) {
+    showToast(translate("Please select transfer order type."));
+    return;
+  }
+
   const productIds = currentOrder.value.items?.map((item: any) => item.productId);
   const productAverageCostDetail = await ProductService.fetchProductsAverageCost(productIds, currentOrder.value.originFacilityId)
 
@@ -609,7 +625,7 @@ async function createOrder() {
     customerId: "COMPANY",
     statusId: "ORDER_CREATED",
     productStoreId: currentOrder.value.productStoreId,
-    statusFlowId: "TO_Fulfill_And_Receive",
+    statusFlowId: currentOrder.value.statusFlowId,
     orderDate: DateTime.now().toFormat("yyyy-MM-dd 23:59:59.000"),
     entryDate: DateTime.now().toFormat("yyyy-MM-dd 23:59:59.000"),
     originFacilityId: currentOrder.value.originFacilityId,
