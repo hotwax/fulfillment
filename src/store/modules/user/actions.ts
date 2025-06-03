df36cf61de99bfd347f113a8d5c8ab5bdd726434
Yash Maheshwari <yashmaheshwari551@gmail.com>
2025-06-03 12:25:43 +0530

    Improved: logic for app view permission and added support to redirect the user to legacy app

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -29,27 +29,32 @@ const actions: ActionTree<UserState, RootState> = {
 
       // Getting the permissions list from server
       const permissionId = process.env.VUE_APP_PERMISSION_ID;
+      const legacyPermissionId = process.env.VUE_APP_LEGACY_PERMISSION_ID;
       // Prepare permissions list
       const serverPermissionsFromRules = getServerPermissionsFromRules();
       if (permissionId) serverPermissionsFromRules.push(permissionId);
+      if (legacyPermissionId) serverPermissionsFromRules.push(legacyPermissionId);
 
       const serverPermissions = await UserService.getUserPermissions({
         permissionIds: [...new Set(serverPermissionsFromRules)]
       }, token);
       const appPermissions = prepareAppPermissions(serverPermissions);
 
-      // Checking if the user has permission to access the app
+      // Checking if the user has permission to access the app or legacy version of the app
       // If there is no configuration, the permission check is not enabled
-      if (permissionId) {
-        // As the token is not yet set in the state passing token headers explicitly
-        // TODO Abstract this out, how token is handled should be part of the method not the callee
-        const hasPermission = appPermissions.some((appPermission: any) => appPermission.action === permissionId );
+      if (permissionId || legacyPermissionId) {
+        const hasPermission = appPermissions.some((appPermission: any) => appPermission.action === permissionId);
+        const hasLegacyPermission = appPermissions.some((appPermission: any) => appPermission.action === legacyPermissionId);
         // If there are any errors or permission check fails do not allow user to login
-        if (!hasPermission) {
+        if (!hasPermission && !hasLegacyPermission) {
           const permissionError = 'You do not have permission to access the app.';
           showToast(translate(permissionError));
           logger.error("error", permissionError);
           return Promise.reject(new Error(permissionError));
+        } else if(hasLegacyPermission && !hasPermission) {
+          // If having the legacy permission then redirect the user, otherwise use the new(current) app
+          // Redirect the user to the same app instance like uat to legacy-uat
+          window.location.href = window.location.href.replace(".hotwax.io", "-legacy.hotwax.io").replace("-uat.hotwax.io", "-legacy-uat.hotwax.io").replace("-dev.hotwax.io", "-legacy-dev.hotwax.io")
         }
       }
 
