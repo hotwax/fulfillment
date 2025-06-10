fdf30f7655c38618b4ebaf2530bf6431f8213585
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2025-06-10 11:40:58 +0530

    Fixed: Used popover for lifecycle dropdown.

--- src/views/CreateTransferOrder.vue
+++ src/views/CreateTransferOrder.vue
@@ -65,7 +65,7 @@
               <ion-card-title>{{ translate("Plan") }}</ion-card-title>
             </ion-card-header>
             <ion-item>
-              <ion-select :label="translate('Lifecycle')" placeholder="Select" v-model="currentOrder.statusFlowId">
+              <ion-select :label="translate('Lifecycle')" placeholder="Select" v-model="currentOrder.statusFlowId" interface="popover">
                 <ion-select-option v-for="flow in statusFlows" :key="flow.statusFlowId" :value="flow.statusFlowId">{{ translate(flow.description) }}</ion-select-option>
               </ion-select>
             </ion-item>
