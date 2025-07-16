9693ce4e5f22a8e576144dc79069feeba3edcc87
Aditya Patel <heyadityapatel@gmail.com>
2025-07-16 13:09:02 +0530

    refactor template and css

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,88 +5,81 @@
         <ion-title>Dashboard</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">
+    <ion-content>
       <!-- Fill Rate Card -->
-      <div class="header">
-        <div>
-          <ion-card>
-            <ion-item lines="none">
-              <p class="overline">14th May Fill Rate</p>
-              <ion-icon slot="end" :icon="informationCircleOutline" />
+      <div class="fulfillment">
+        <ion-card class="fill-rate">
+          <ion-item lines="none">
+            <p class="overline">14th May Fill Rate</p>
+            <ion-icon slot="end" :icon="informationCircleOutline" />
+          </ion-item>
+          <ion-list lines="none">
+            <h1>23%</h1>
+            <ion-item>
+              <ion-label>Order allocated</ion-label>
+              <ion-label slot="end">10/14</ion-label>
             </ion-item>
-            <ion-list lines="none">
-              <ion-item>
-                <h1 class="order-count">23%</h1>
-              </ion-item>
-              <ion-item>
-                <ion-label>Order allocated</ion-label>
-                <ion-label slot="end">10/14</ion-label>
-              </ion-item>
-              <ion-item>
-                <ion-label>Orders packed</ion-label>
-                <ion-label slot="end" color="success">6</ion-label>
-              </ion-item>
-              <ion-item>
-                <ion-label>Orders rejected</ion-label>
-                <ion-label slot="end" color="danger">1</ion-label>
-              </ion-item>
-            </ion-list>
-          </ion-card>   
-        </div>
+            <ion-item>
+              <ion-label>Orders packed</ion-label>
+              <ion-label slot="end" color="success">6</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Orders rejected</ion-label>
+              <ion-label slot="end" color="danger">1</ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-card>
 
-        <div class="orders">
-          <!-- Orders Pending Fulfillment Card -->
-          <ion-card>
-            <ion-item lines="none">
-              <p class="overline">Orders Pending Fulfillment</p>
-            </ion-item>
-            <ion-item lines="none" class="fulfillment-card ">
-              <div slot="start" class="left-info">
-                <h1 class="order-count">3</h1>
-                <ion-label class="ion-margin-top">
+        <!-- Orders Pending Fulfillment Card -->
+        <ion-card class="orders">
+          <p class="overline">Orders Pending Fulfillment</p>
+          <div class="pending">
+            <h1>3</h1>
+            <ion-item>
+                <ion-label>
                   <p>Oldest order assigned</p>
                   1 hour 10 minutes ago
                 </ion-label>
-              </div>
-              <div class="right-info">
-                <ion-item detail>
-                  <ion-icon :icon="mailUnreadOutline" slot="start" />
-                  <ion-label>1 open</ion-label>
-                </ion-item>
-                <ion-item detail lines="none">
-                  <ion-icon :icon="mailOpenOutline" slot="start" />
-                  <ion-label>2 in progress</ion-label>
-                </ion-item>
-              </div>
             </ion-item>
-          </ion-card>
-
-          <OrderStatusBar
-            :total="14"
-            :dataSegments="{
-              Packed: { value: 6, color: '#3FBF60' },
-              Rejected: { value: 1, color: '#EF5350' },
-              Allocated: { value: 4, color: '#3880FF' }
-            }"
-          />
-
-          <!-- Notifications -->
-          <div class="notification-items">
-            <ion-item lines="none" class="item-box">
-              <ion-icon slot="start" :icon="sendOutline" color="warning" />
-              <ion-label>
-                Carrier pickup scheduled
-                <p>04:30pm</p>
-              </ion-label>
-            </ion-item>
-            <ion-item lines="none" class="item-box">
-              <ion-icon slot="start" :icon="storefrontOutline" color="danger" />
-              <ion-label>
-                Store closes in 30 minutes
-                <p>04:30pm</p>
-              </ion-label>
-            </ion-item> 
           </div>
+          <div class="fulfill">
+            <ion-item lines="full" detail>
+              <ion-icon :icon="mailUnreadOutline" slot="start" />
+              <ion-label>1 open</ion-label>
+            </ion-item>
+            <ion-item lines="none" detail>
+              <ion-icon :icon="mailOpenOutline" slot="start" />
+              <ion-label>2 in progress</ion-label>
+            </ion-item>
+          </div>
+        </ion-card>
+
+        <OrderStatusBar 
+          :total="14"
+          :dataSegments="{
+            Packed: { value: 6, color: '#3FBF60' },
+            Rejected: { value: 1, color: '#EF5350' },
+            Allocated: { value: 4, color: '#3880FF' }
+          }"
+        />
+        <!-- rename component to "FulfillmentProgressBar" -->
+
+        <!-- Scheduling -->
+        <div class="scheduling">
+          <ion-item lines="none">
+            <ion-icon slot="start" :icon="sendOutline" color="warning" />
+            <ion-label>
+              Carrier pickup scheduled
+              <p>04:30pm</p>
+            </ion-label>
+          </ion-item>
+          <ion-item lines="none">
+            <ion-icon slot="start" :icon="storefrontOutline" color="danger" />
+            <ion-label>
+              Store closes in 30 minutes
+              <p>04:30pm</p>
+            </ion-label>
+          </ion-item> 
         </div>
       </div>
     </ion-content>
@@ -114,51 +107,47 @@ import OrderStatusBar from '@/components/OrderStatusBar.vue'
 </script>
 
 <style scoped>
-.order-count {
-  font-size: 78px;
-  font-weight: 400;
-  margin: 0;
+
+
+/* add media query for desktop only */
+.fulfillment {
+  display: grid;
+  grid-template-areas: 
+  "fill-rate orders"
+  "fill-rate progress-bar"
+  "fill-rate scheduling";
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: 1fr auto auto;
 }
 
-.header {
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  gap: 1rem;
+.fill-rate {
+  grid-area: fill-rate;
+}
+
+.fill-rate h1, .orders h1 {
+  font-size: 78px;
+  padding-inline: var(--spacer-sm);
 }
 
 .orders {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
+  grid-area: orders;
+  display: grid;
+  grid-template-areas:
+  "title title"
+  "pending fulfill";
+  grid-template-columns: auto 343px;
+  grid-template-rows: min-content auto;
 }
 
-.fulfillment-card {
-  display: flex;
+OrderStatusBar {
+  grid-area: progress-bar;
 }
 
-.left-info {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  flex: 1.5;
+.scheduling {
+  grid-area: scheduling;
 }
 
-.right-info {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-}
-
-.notification-items {
-  display: flex;
-  gap: 1rem;
-}
-
-.notification-items ion-item {
-  flex: 1;
-}
-
-.item-box::part(native) {
+.scheduling ion-item::part(native) {
   --border-radius: var(--spacer-xs);
   border: var(--border-medium);
 }
