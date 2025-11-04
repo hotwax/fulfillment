df7a286d2012f09073cacca8e5f02a6398b68646
R-Sourabh <rsourabh002@gmail.com>
2025-11-04 16:17:16 +0530

    Improved: added radix and NaN handling for picked quantity parsing (#1400)

--- src/components/TransferOrderItem.vue
+++ src/components/TransferOrderItem.vue
@@ -254,7 +254,7 @@ export default defineComponent({
       if(this.currentOrder.statusId !== 'ORDER_CREATED') return;
 
       const currentItem = this.currentOrder.items.find((orderItem: any) => orderItem.orderItemSeqId === item.orderItemSeqId);
-      const itemQuantity = parseInt((this.$refs.pickedQuantity as any).$el.value)
+      const itemQuantity = parseInt((this.$refs.pickedQuantity as any).$el.value) || 0
 
       // Skip if picked quantity is same as current or invalid (equal to or less than 0)
       if(currentItem && itemQuantity === currentItem.quantity) return;
